5782deb3cc36c1696c494afc2d107c9d453065a7
wangmeng <wangmeng647@outlook.com>
2023-08-24 12:35:48 +0800

    addcom

--- ww.py
+++ ww.py
@@ -1,3 +1,8 @@
-
-print(1)
-print(222)
+import re
+text = 'qwedas qweqwasd rqweq'
+res = re.findall(r"[a-zA-Z0-9]+", text)
+print(list(res))
+ls = [1,2,3,4]
+se = {1,2}
+res = list(filter(lambda x: x in se, ls))
+print(res)
